792dcd0c1fa4c581e91fc50cd1c546ed6dc2efe6
lorenzoVwebDev <lorenzoviganego.work@libero.it>
2025-01-15 21:52:21 +0100

    updated

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,8 @@
-
+import Counter from '../Counter/Counter';
 
 function App() {
   return (
-    <></>
+    <Counter/>
   );
 }
 
--- src/components/Store/Store.js
+++ src/components/Store/Store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../../features/counter/counterSlice';
 
-export default configureStore({
+const store = configureStore({
   reducer: {
-    counter: counterReducer
+    counter: counterReducer,
   },
 })
+
+export default store;
